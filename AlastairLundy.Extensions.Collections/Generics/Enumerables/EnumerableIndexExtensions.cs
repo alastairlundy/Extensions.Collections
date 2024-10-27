19696a9ae67c3e8b25611da7145203fbf42975b1
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-10-27 12:06:23 +0000

    Update EnumerableIndexExtensions.cs

--- AlastairLundy.Extensions.Collections/Generics/Enumerables/EnumerableIndexExtensions.cs
+++ AlastairLundy.Extensions.Collections/Generics/Enumerables/EnumerableIndexExtensions.cs
@@ -24,6 +24,7 @@
 
 using System.Collections.Generic;
 using System.Linq;
+
 using AlastairLundy.Extensions.Collections.Specializations.Indexes;
 
 namespace AlastairLundy.Extensions.Collections.IEnumerables
