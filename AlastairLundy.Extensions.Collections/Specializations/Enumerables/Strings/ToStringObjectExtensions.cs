0ca3d4c90d4d95fd72bc96411d670f70f60583ae
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-11-10 21:38:13 +0000

    add enumerable ToString extension

---
+++ AlastairLundy.Extensions.Collections/Specializations/Enumerables/Strings/ToStringObjectExtensions.cs
@@ -0,0 +1,91 @@
+﻿/*
+        MIT License
+       
+       Copyright (c) 2024 Alastair Lundy
+       
+       Permission is hereby granted, free of charge, to any person obtaining a copy
+       of this software and associated documentation files (the "Software"), to deal
+       in the Software without restriction, including without limitation the rights
+       to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+       copies of the Software, and to permit persons to whom the Software is
+       furnished to do so, subject to the following conditions:
+       
+       The above copyright notice and this permission notice shall be included in all
+       copies or substantial portions of the Software.
+       
+       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+       IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+       FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+       AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+       LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+       OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+       SOFTWARE.
+   */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlastairLundy.Extensions.Collections.Specializations.Strings;
+
+public static class ToStringObjectExtensions
+{
+    /// <summary>
+    /// Converts an IEnumerable of strings to a string separated by a separator string.
+    /// </summary>
+    /// <param name="source">The enumerable to be turned into a string.</param>
+    /// <param name="sourceItemSeparator">The string to separate the items in the source enumerable.</param>
+    /// <returns>the string containing all the strings in the source enumerable separated by the separator.</returns>
+    public static string ToString(this IEnumerable<string> source, string sourceItemSeparator)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        foreach (string item in source)
+        {
+            stringBuilder.Append(item);
+
+            if (sourceItemSeparator == Environment.NewLine)
+            {
+                stringBuilder.AppendLine();
+            }
+            else
+            {
+                stringBuilder.Append(sourceItemSeparator);
+            }
+        }
+
+        stringBuilder = stringBuilder.Remove(stringBuilder.Length, stringBuilder.Length - sourceItemSeparator.Length);
+        
+        return stringBuilder.ToString();
+    }
+
+    /// <summary>
+    /// Converts an IEnumerable of objects of Type T to a string separated by a separator string.
+    /// </summary>
+    /// <param name="source">The enumerable to be turned into a string.</param>
+    /// <param name="sourceItemSeparator">The string to separate the items in the source enumerable.</param>
+    /// <typeparam name="T">The type of objects to be enumerated.</typeparam>
+    /// <returns>the string containing all the strings in the source enumerable separated by the separator.</returns>
+    public static string ToString<T>(this IEnumerable<T> source, string sourceItemSeparator)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        foreach (T item in source)
+        {
+            stringBuilder.Append(item.ToString());
+
+            if (sourceItemSeparator == Environment.NewLine)
+            {
+                stringBuilder.AppendLine();
+            }
+            else
+            {
+                stringBuilder.Append(sourceItemSeparator);
+            }
+        }
+
+        stringBuilder = stringBuilder.Remove(stringBuilder.Length, stringBuilder.Length - sourceItemSeparator.Length);
+        
+        return stringBuilder.ToString();
+    }
+}
