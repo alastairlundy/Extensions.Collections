19222c3547a1681587bcf18ac3fa1ac85a5dafdd
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-11-25 15:03:43 +0000

    create replacement string and char methods

--- AlastairLundy.Extensions.Collections/Generics/Enumerables/EnumerableIndexExtensions.cs
+++ AlastairLundy.Extensions.Collections/Generics/Enumerables/EnumerableIndexExtensions.cs
@@ -22,10 +22,13 @@
        SOFTWARE.
    */
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 using AlastairLundy.Extensions.Collections.Specializations.Indexes;
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable SuggestVarOrType_BuiltInTypes
 
 namespace AlastairLundy.Extensions.Collections.IEnumerables
 {
@@ -63,6 +66,7 @@ namespace AlastairLundy.Extensions.Collections.IEnumerables
         /// <param name="obj">The item to search for.</param>
         /// <typeparam name="T"></typeparam>
         /// <returns>The indexes if the object is found; a single element Enumerable with a value of -1 otherwise.</returns>
+        
         public static IEnumerable<int> IndexesOf<T>(this IEnumerable<T> source, T obj)
         {
             if (typeof(T) == typeof(string))
@@ -84,16 +88,73 @@ namespace AlastairLundy.Extensions.Collections.IEnumerables
                         indexes.Add(index);
                     }
                 }
+        
+        /// <summary>
+        /// Gets the indexes of the specified string within an IEnumerable of strings.
+        /// </summary>
+        /// <param name="source">The IEnumerable of strings to be searched.</param>
+        /// <param name="expected">The string to look for.</param>
+        /// <param name="ignoreCase">Whether to ignore the case of the expected string.</param>
+        /// <returns>The indexes if the string is found; an empty IEnumerable otherwise.</returns>
+        public static IEnumerable<(int indexWithinEnumerable, int indexWithinString)> IndexesOf(this IEnumerable<string> source,
+            string expected, bool ignoreCase = true)
+        {
+            List<(int, int)> indexes = new List<(int, int)>();
 
-                if (indexes.Count > 0)
+            string[] enumerable = source as string[] ?? source.ToArray();
+            
+            for(int enumerableIndex = 0; enumerableIndex < enumerable.Length; enumerableIndex++)
+            {
+                if (enumerable[enumerableIndex].Contains(expected))
                 {
-                    return indexes; 
-                }
-                else
-                {
-                    return new[]{-1};
+                    IEnumerable<int> tempIndexes =
+                        // ReSharper disable once RedundantNameQualifier
+                        AlastairLundy.Extensions.System.Strings.Indexes.IndexesOfExtensions.IndexesOf(
+                            enumerable[enumerableIndex], expected, ignoreCase);
+
+                    tempIndexes = tempIndexes.Where(i => i != -1);
+                    
+                    indexes.AddRange(tempIndexes.Select(item => (enumerableIndex, item)));
                 }
             }
+            
+            indexes = indexes.DeDuplicate().ToList();
+            
+            return indexes;
+        }
+        
+        /// <summary>
+        /// Gets the indexes of the specified char within an IEnumerable of strings.
+        /// </summary>
+        /// <param name="source">The IEnumerable of strings to be searched.</param>
+        /// <param name="expected">The char to look for.</param>
+        /// <param name="ignoreCase">Whether to ignore the case of the expected char.</param>
+        /// <returns>The indexes if the char is found; an empty IEnumerable otherwise.</returns>
+        public static IEnumerable<(int indexWithinEnumerable, int indexWithinString)> IndexesOf(this IEnumerable<string> source,
+            char expected, bool ignoreCase = true)
+        {
+            List<(int, int)> indexes = new List<(int, int)>();
+
+            string[] enumerable = source as string[] ?? source.ToArray();
+            
+            for(int enumerableIndex = 0; enumerableIndex < enumerable.Length; enumerableIndex++)
+            {
+                if (enumerable[enumerableIndex].ToLower().Equals(expected.ToString().ToLower()))
+                {
+                    IEnumerable<int> tempIndexes =
+                        // ReSharper disable once RedundantNameQualifier
+                        AlastairLundy.Extensions.System.Strings.Indexes.IndexesOfExtensions.IndexesOf(
+                            enumerable[enumerableIndex], expected, ignoreCase);
+
+                    tempIndexes = tempIndexes.Where(i => i != -1);
+                    
+                    indexes.AddRange(tempIndexes.Select(item => (enumerableIndex, item)));
+                }
+            }
+            
+            indexes = indexes.DeDuplicate().ToList();
+            
+            return indexes;
         }
     }
 }
