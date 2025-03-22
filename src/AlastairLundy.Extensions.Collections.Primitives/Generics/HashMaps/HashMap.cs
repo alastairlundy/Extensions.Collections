f6fdfb69efefd9fa13774a784eff0e31d75cb0de
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-03-22 15:17:50 +0000

    Update HashMap.cs

--- src/AlastairLundy.Extensions.Collections.Primitives/Generics/HashMaps/HashMap.cs
+++ src/AlastairLundy.Extensions.Collections.Primitives/Generics/HashMaps/HashMap.cs
@@ -26,6 +26,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+
 using AlastairLundy.Extensions.Collections.Primitives.Exceptions;
 
 // ReSharper disable ClassNeverInstantiated.Global
