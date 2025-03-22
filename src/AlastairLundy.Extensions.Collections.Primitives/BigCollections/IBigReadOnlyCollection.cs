d0f9bd878471b3edf8244e88cab26d39a6b4b6a0
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-03-22 15:06:09 +0000

    start work on IBigReadOnlyCollection

---
+++ src/AlastairLundy.Extensions.Collections.Primitives/BigCollections/IBigReadOnlyCollection.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace AlastairLundy.Extensions.Collections.Primitives.BigCollections;
+
+public interface IBigReadOnlyCollection<T> : IEnumerable<T>
+{
+    
+}
