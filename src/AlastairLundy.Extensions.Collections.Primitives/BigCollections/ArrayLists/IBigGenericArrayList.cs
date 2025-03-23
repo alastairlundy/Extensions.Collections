53c73e4172470651f0192f51038bd480cfa35c07
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-03-23 13:26:26 +0000

    use block scoped namespaces

--- src/AlastairLundy.Extensions.Collections.Primitives/BigCollections/ArrayLists/IBigGenericArrayList.cs
+++ src/AlastairLundy.Extensions.Collections.Primitives/BigCollections/ArrayLists/IBigGenericArrayList.cs
@@ -24,96 +24,97 @@
 
 using System.Collections.Generic;
 
-namespace AlastairLundy.Extensions.Collections.Primitives.BigCollections;
-
-/// <summary>
-/// 
-/// </summary>
-/// <typeparam name="T"></typeparam>
-public interface IBigGenericArrayList<T> : IBigList<T>
+namespace AlastairLundy.Extensions.Collections.Primitives.BigCollections
 {
-
-    bool IsFixedSize { get; }
-
-    bool IsSynchronized { get; }
-    
-    long Capacity { get; }
-    
-    void AddRange(ICollection<T> collection);
-     
-    long BinarySearch(long index, long count, T value, IComparer<T> comparer); 
-    long BinarySearch(T value, IComparer<T> comparer);
-
-    void CopyTo(BigArray<T> array);
-    void CopyTo(long index, BigArray<T> array, long arrayIndex, long count);
-     
-    IBigGenericArrayList<T> FixedSize(IBigGenericArrayList<T> source);
-    IBigList<T> FixedSize(IBigList<T> source);
-     
-    IBigGenericArrayList<T> GetRange(long index, long count);
-
-    long IndexOf(T? value, long startIndex);
-    long IndexOf(T? value, long startIndex, long count);
-
-    void InsertRange(long index, ICollection<T> collection);
-     
-    long LastIndexOf(T value);
-    long LastIndexOf(T value, long startIndex);
-    long LastIndexOf(T value, long startIndex, long count);
-     
-    IBigGenericArrayList<T> ReadOnly(IBigGenericArrayList<T> source);
-    IList<T> ReadOnly(IList<T> source);
-
-    void RemoveRange(long index, long count);
-     
-    IBigGenericArrayList<T> Repeat(T value, long count);
-     
     /// <summary>
     /// 
     /// </summary>
-    void Reverse();
+    /// <typeparam name="T"></typeparam>
+    public interface IBigGenericArrayList<T> : IBigList<T>
+    {
+
+        bool IsFixedSize { get; }
+
+        bool IsSynchronized { get; }
+    
+        long Capacity { get; }
+    
+        void AddRange(ICollection<T> collection);
+     
+        long BinarySearch(long index, long count, T value, IComparer<T> comparer); 
+        long BinarySearch(T value, IComparer<T> comparer);
+
+        void CopyTo(BigArray<T> array);
+        void CopyTo(long index, BigArray<T> array, long arrayIndex, long count);
+     
+        IBigGenericArrayList<T> FixedSize(IBigGenericArrayList<T> source);
+        IBigList<T> FixedSize(IBigList<T> source);
+     
+        IBigGenericArrayList<T> GetRange(long index, long count);
+
+        long IndexOf(T? value, long startIndex);
+        long IndexOf(T? value, long startIndex, long count);
+
+        void InsertRange(long index, ICollection<T> collection);
+     
+        long LastIndexOf(T value);
+        long LastIndexOf(T value, long startIndex);
+        long LastIndexOf(T value, long startIndex, long count);
+     
+        IBigGenericArrayList<T> ReadOnly(IBigGenericArrayList<T> source);
+        IList<T> ReadOnly(IList<T> source);
+
+        void RemoveRange(long index, long count);
+     
+        IBigGenericArrayList<T> Repeat(T value, long count);
+     
+        /// <summary>
+        /// 
+        /// </summary>
+        void Reverse();
         
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="index"></param>
-    /// <param name="count"></param>
-    void Reverse(long index, long count);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="count"></param>
+        void Reverse(long index, long count);
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="index"></param>
-    /// <param name="collection"></param>
-    void SetRange(long index, ICollection<T> collection);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="collection"></param>
+        void SetRange(long index, ICollection<T> collection);
     
-    /// <summary>
-    /// 
-    /// </summary>
-    void Sort();
+        /// <summary>
+        /// 
+        /// </summary>
+        void Sort();
         
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="comparer"></param>
-    void Sort(IComparer<T> comparer);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="comparer"></param>
+        void Sort(IComparer<T> comparer);
         
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="index"></param>
-    /// <param name="count"></param>
-    /// <param name="comparer"></param>
-    void Sort(long index, long count, IComparer<T> comparer);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="count"></param>
+        /// <param name="comparer"></param>
+        void Sort(long index, long count, IComparer<T> comparer);
     
-    IBigGenericArrayList<T> Synchronized(IBigGenericArrayList<T> source);
+        IBigGenericArrayList<T> Synchronized(IBigGenericArrayList<T> source);
     
-    IBigList<T> Synchronized(IBigList<T> source);
+        IBigList<T> Synchronized(IBigList<T> source);
     
-    BigArray<T> ToArray();
+        BigArray<T> ToArray();
     
-    /// <summary>
-    /// 
-    /// </summary>
-    void TrimToSize();
+        /// <summary>
+        /// 
+        /// </summary>
+        void TrimToSize();
+    }
 }
